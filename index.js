0255938d9f1154797bd9ab2eb3b10df8406b16f5
pieceofwater <kriegg878@gmail.com>
2020-10-18 21:22:16 +0300

    update

--- index.js
+++ index.js
@@ -9,21 +9,24 @@ let token = tokenObj.token;
 const { Telegraf } = require('telegraf');
 const bot = new Telegraf(token);
 
+//array with all taked words (later it will be in a separate file)
 let messages = [];
 
+//shuffling the array when generating phrase 
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
 
       [array[i], array[j]] = [array[j], array[i]];
     }
-  }
+}
 
 function randomInteger(min, max) {
     let rand = min - 0.5 + Math.random() * (max - min + 1);
     return Math.round(rand);
 }
 
+//taking message and push words into array
 function takeMsg(ctx) {
     let message = ctx.message.text;
     let splittedPhrase = message.split(' ');
@@ -33,9 +36,9 @@ function takeMsg(ctx) {
     });
 
     return;
-
 }
 
+//generating phrase
 function generate(ctx) {
     if(messages.length <= 6) {
         ctx.reply('Недостаточно данных для генерации фразы.');
@@ -44,21 +47,18 @@ function generate(ctx) {
 
     shuffle(messages);
 
-    let words = randomInteger(1, 10);
+    let words = randomInteger(1, 10); //a number of words in the phrase
     let phraseArr = [];
     let count = 0;
 
     for(let msg of messages) {
-
         let skipChance = randomInteger(1, 10);
         if(skipChance <= 7) continue;
 
         phraseArr.push(msg);
 
         count++;
-        if(count >= words) {
-            break;
-        }
+        if(count >= words) break;
     }
 
     let phrase = phraseArr.join(' ');
@@ -72,7 +72,7 @@ function dbg(ctx) {
 
 bot.start((ctx) => {
     ctx.reply('started');
-}); 
+});
 
 bot.command('gen', generate);
 bot.command('dbg', dbg);
